29b2442bf4b6a57e5d7fddc0c2b3fc2d7a9e1daf
Otto Gugel <harpgamer31@gmail.com>
2025-02-18 17:17:39 -0300

    Aula 02

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/app/components/button";
 import { TechBadge } from "@/app/components/tech-badge";
 import Image from "next/image"
+import { HiArrowNarrowRight } from 'react-icons/hi'
+import { TbBrandGithub, TbBrandGmail, TbBrandLinkedin } from "react-icons/tb";
+
+const MOCK_CONTACTS = [
+  {
+    url: "https://github.com.br",
+    icon: <TbBrandGithub />,
+  },
+  {
+    url: "https://linkedin.com.br",
+    icon: <TbBrandLinkedin />,
+  },
+  {
+    url: "mailto:ottogugel99@gmail.com",
+    icon: <TbBrandGmail />,
+  },
+];
 
 export const HeroSection = () => {
   return (
-    <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
+    <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
         <div className="w-full lg:max-w-[530px]">
           <p className="font-mono text-blue-500">Hello, my name is</p>
@@ -22,7 +40,25 @@ export const HeroSection = () => {
             ))}
           </div>
 
-          <div>contato</div>
+          <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
+            <Button className="w-max shadow-button">
+              Entre em contato
+              <HiArrowNarrowRight size={10} />
+            </Button>
+
+            <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
+              {MOCK_CONTACTS.map((contact, index) => (
+                <a
+                href={contact.url}
+                key={`contact-${index}`}
+                target="_blank"
+                className="hover:text-gray-100 transition-colors"
+                >
+                  {contact.icon}
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
 
         <Image
@@ -30,6 +66,7 @@ export const HeroSection = () => {
           height={404}
           src="/images/profile-pic.png"
           alt="Foto do perfil do Otto Gugel"
+          className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
         />
       </div>
     </section>
---
+++ app/components/pages/home/known-techs/index.tsx
@@ -0,0 +1,23 @@
+import { TbBrandNextjs } from "react-icons/tb"
+import { SectionTitle } from "../section-title"
+import { KnownTech } from "./known-tech"
+
+export const KnowTechs = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="expertise" title="Knowledge" />
+
+      <div className="grid grid-cols-[repeat(auto-fit, minmax(264px, 1fr))] gap-3 mt-[60px]">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <KnownTech
+            tech={{
+              icon: <TbBrandNextjs />,
+              name: "Next.js",
+              startDate: "2025-01-20",
+            }}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
