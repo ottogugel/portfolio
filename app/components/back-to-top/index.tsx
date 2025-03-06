44ff8ab754ed7d96ee18aa60ead9573f0decd2c8
ottogugel <harpgamer31@gmail.com>
2025-03-06 13:44:07 -0300

    Aula 08 - Parte 2

--- app/components/back-to-top/index.tsx
+++ app/components/back-to-top/index.tsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 
-import { TbArrowNarrowUp } from "react-icons/tb"
-import { Button } from "../button"
-import { useCallback, useEffect, useState } from "react"
+import { TbArrowNarrowUp } from "react-icons/tb";
+import { useCallback, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "../button";
 
 export const BackToTop = () => {
-  const [show, setShow] = useState(false)
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   const handleScroll = useCallback(() => {
     if (!show && window.scrollY > 500) {
@@ -26,11 +26,22 @@ export const BackToTop = () => {
   }, [handleScroll]);
 
   return (
-    <div className="fixed right-4 bottom-4 z-20">
-      <Button onClick={scrollToTop} className="shadow-lg shadow-blue-400/20">
-       <TbArrowNarrowUp size={20} />
-      </Button>
-
-    </div>
-  )
-}
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className="fixed bottom-4 right-4 z-20"
+          initial={{ opacity: 0, right: -5 }}
+          animate={{ opacity: 1, right: 16 }}
+          exit={{ opacity: 0, right: -5 }}
+        >
+          <Button
+            onClick={handleClick}
+            className="shadow-lg shadow-blue-400/20"
+          >
+            <TbArrowNarrowUp size={20} />
+          </Button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
